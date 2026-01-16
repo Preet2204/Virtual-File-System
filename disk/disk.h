aab908df5c28a0f8d07ce16e1e8b364fcef66d64
Preet2204 <preetsiddhapura2204@gmail.com>
2026-01-16 15:55:04 +0530

    Completed disk.h Implementation

--- disk/disk.h
+++ disk/disk.h
@@ -51,12 +51,72 @@ DiskManager::DiskManager(std::string diskImagePath_) : diskImagePath(diskImagePa
 
 DiskManager::readBlock(uint32_t blockNum, void* buffer) {
 
+    if(buffer == nullptr) {
+        throw std::invalid_argument(std::string("buffer is nullptr"));
+    }
+
+    if (blockNum >= numBlocks) {
+        throw std::invalid_argument(std::string("blockNum is >= total number of block: ") + std::to_string(blockNum) + std::string(" > ") + std::to_string(numBlocks));
+    }
+
+    char* bufferChar = static_cast<char*>(buffer);
+
+    disk.clear();
+
+    disk.seekg(static_cast<std::uint64_t>(blockNum) * blockSize, std::ios::beg);
+
+    if(!disk.good()) {
+        throw std::runtime_error(std::string("disk.seekg() was failed."));
+    }
+
+    disk.read(bufferChar, blockSize);
+
+    if(disk.gcount() < blockSize) {
+        throw std::runtime_error(std::string("Only Partial block was read: ") + std::to_string(disk.gcount()) + std::string(" bytes"));
+    }
+
 }
 
 DiskManager::writeBlock(uint32_t blockNum, void* buffer) {
 
+    if (buffer == nullptr) {
+        throw std::invalid_argument(std::string("buffer is nullptr"));
+    }
+
+    if (blockNum >= numBlocks) {
+        throw std::invalid_argument(std::string("blockNum is >= total number of block: ") + std::to_string(blockNum) + std::string(" > ") + std::to_string(numBlocks));
+    }
+
+    char* bufferChar = static_cast<char*>(buffer);
+
+    disk.clear();
+    disk.seekp(static_cast<uint64_t>(blockNum) * blockSize, std::ios::beg);
+
+    if(!disk.good()) {
+        throw std::runtime_error(std::string("disk.seekp() was failed."));
+    }
+
+    disk.write(bufferChar, blockSize);
+
+    if(!disk.good()) {
+        throw std::runtime_error(std::string("disk.write() was failed."));
+    }
+
+    disk.flush();
+
 }
 
 DiskManager::formatDisk() {
+
+    char* bufferChar = new char[blockSize];
+
+    std::fill(bufferChar, bufferChar + blockSize, 0);
+    void* buffer = static_cast<void*>(bufferChar);
+
+    for(uint32_t i = 0; i < numBlocks; ++i) {
+        writeBlock(i, buffer);
+    }
+
+    delete[] bufferChar;
 
 }
