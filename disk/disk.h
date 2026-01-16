c75cb5e60d7daddd6dab0efa821b8858baa92a5c
Preet2204 <preetsiddhapura2204@gmail.com>
2026-01-16 09:45:12 +0530

    Added Constructor of DiskManager

--- disk/disk.h
+++ disk/disk.h
@@ -1,15 +1,29 @@
 #include <iostream>
+#include <string>
 
 class DiskManager {
-    uint16_t blockSize = 4096;
-    uint16_t numBlocks = 2048;
+    const uint16_t blockSize;
+    const uint16_t numBlocks;
 
-
+    std::string diskImagePath;
+    std::fstream disk;
 
     void readBlock(uint32_t blockNum, void* buffer);
     void writeBlock(uint32_t blockNum, void* buffer);
     void formatDisk();
 }
+
+DiskManager::DiskManager(std::string diskImagePath_) {
+    
+    diskImagePath = diskImagePath_;
+    disk.open(diskImagePath, disk.binary | disk.in | disk.out);
+
+    if(!disk.is_open()) {
+	
+    }
+
+}
+
 
 DiskManager::readBlock(uint32_t blockNum, void* buffer) {
 
