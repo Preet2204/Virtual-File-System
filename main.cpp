8b1868ddcd5daebf8d035aaedd76e48c1ee72356
Preet2204 <preetsiddhapura2204@gmail.com>
2026-01-17 14:54:24 +0530

    Revert "Started filesystem implementation, and completed mkfs"
    This reverts commit 3861143746593c3b9afa65fa488c5d49f8354491.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
 // Virtual File System 
 #include <iostream>
-#include <string>
-#include "fs/fs.cpp"
 
 int main () {
-
-    try {
-        mkfs("vdisk.img");
-        std::cout << "Successful" << '\n';
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << '\n';
-    }
-
-
+    std::cout << "Hello World!" << '\n';
 }
